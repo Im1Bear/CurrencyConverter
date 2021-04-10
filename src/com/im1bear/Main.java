794225f8ba5bd16ee780caa525d6f95c3baf700d
Alex <alexander-bethfuehr@gmx.de>
2021-04-10 12:51:23 +0200

    gitignore + main class

--- src/com/im1bear/Main.java
+++ src/com/im1bear/Main.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int Number;
+        long Dollar;
+        long Bitcoin;
+        long Euro;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("What do you want to do? \n" +
                 "1. Convert $ to €\n" +
@@ -13,11 +18,18 @@ public class Main {
                 "4. Convert GBP to €\n" +
                 "5. Convert $ to GPB" +
                 "6. Convert GBP to Bitcoin");
-        int Number = scanner.nextInt();
+        Number = scanner.nextInt();
         if (Number == 1) {
             System.out.println("Enter how much you want to convert: ");
-            long Dollar = scanner.nextInt();
+            Dollar = scanner.nextLong();
             System.out.println("Euros: "+ Dollar * 1.19);
+        } else if (Number == 2) {
+            System.out.println("Enter how much you want to convert: ");
+            Dollar = scanner.nextLong();
+            System.out.println("Bitcoin: " + Dollar * .000016);
+        } else if (Number == 3) {
+            System.out.println("Enter how much you want to convert: ");
+            Euro = scanner.nextLong();
         }
     }
 }
