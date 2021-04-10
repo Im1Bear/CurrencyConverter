1b3ed23a9d6483ea3790d5e18453ed6d73ede819
Alex <alexander-bethfuehr@gmx.de>
2021-04-10 13:23:01 +0200

    Added Comments

--- src/com/im1bear/Main.java
+++ src/com/im1bear/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         int Number;
         long Dollar;
-        long Bitcoin;
         long Euro;
+        long GBP;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("What do you want to do? \n" +
@@ -18,18 +18,32 @@ public class Main {
                 "4. Convert GBP to €\n" +
                 "5. Convert $ to GPB" +
                 "6. Convert GBP to Bitcoin");
+        System.out.println("Enter a Number: ");
         Number = scanner.nextInt();
         if (Number == 1) {
+            // Option Nr. 1
+            // Converting Dollar to Euros
             System.out.println("Enter how much you want to convert: ");
             Dollar = scanner.nextLong();
             System.out.println("Euros: "+ Dollar * 1.19);
         } else if (Number == 2) {
+            // Option Nr. 2
+            /*
+            Converting Dollar into Bitcoin
+            ( Not Always accurate )
+             */
             System.out.println("Enter how much you want to convert: ");
             Dollar = scanner.nextLong();
-            System.out.println("Bitcoin: " + Dollar * .000016);
+            System.out.println("Bitcoin: " + Dollar * 0.000016);
         } else if (Number == 3) {
+            // Option Nr. 3
+            // Converting Euro to Bitcoin
             System.out.println("Enter how much you want to convert: ");
             Euro = scanner.nextLong();
+            System.out.println("Bitcoin: " + Euro * 0.000020);
+        } else if (Number == 4) {
+            System.out.println("Enter how much you want to convert: ");
+
         }
     }
 }
